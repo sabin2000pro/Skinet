175a839b173666ba4210c6576d9569783e0361bc
sabin2000pro <sabinlungu293@gmail.com>
2024-12-18 18:23:40 +0000

    Added config to the Docker Compose file to build and run the SQL Server image with the port and environment

--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd7bf6a30e677a582ef2dfc0c80a79f87c4b0872")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e81a1686bc1c329670e5b9ae91cc546c6058ce1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
