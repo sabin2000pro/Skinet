1f6b69efa38ffd27e323bcccfe51bc53c7bc4b9b
sabin2000pro <sabinlungu293@gmail.com>
2024-12-18 17:46:42 +0000

    Adding Product Entity attributes such as Name, Description, Price, Type, Brand etc..

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -4,6 +4,14 @@ public class Product {
     public required string Name {get; set; }
     
     public required string Description {get; set; }
+    public required decimal Price {get; set; }
+
+    public required string PictureUrl {get; set; }
+
+    public required string Type {get; set; }
+
+    public required string Brand {get; set; }
 
     public required int QuantityInStock {get; set; }
+
 }
