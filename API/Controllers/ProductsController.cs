1cf4813e63391b833f5298ccb7b690fabfe816a7
sabin2000pro <sabinlungu293@gmail.com>
2024-12-18 18:56:27 +0000

    Implemented the function for returning the list of Products

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,8 +1,26 @@
+using Core;
+using Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API;
 
 [ApiController]
+[Route("api/[controller]")]
 public class ProductsController : ControllerBase {
+
+    private readonly StoreContext context;
+
+    public ProductsController(StoreContext _context)
+
+    {
+        _context = context;
+    }
     
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> FetchTasks() {
+        return await context.Product.ToListAsync();
+    }
+    
+
 }
